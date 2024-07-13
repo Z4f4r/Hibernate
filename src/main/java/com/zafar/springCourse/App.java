3d48882aeb9457e5e30c484c7d3828b7b73d7e4c
Zafarjon <idk677799@gmail.com>
2024-07-13 20:35:48 +0300

    1-3

---
+++ src/main/java/com/zafar/springCourse/App.java
@@ -0,0 +1,37 @@
+package com.zafar.springCourse;
+
+import com.zafar.springCourse.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+//            Person person = session.get(Person.class, 1);
+//            System.out.println(person.getName());
+//            System.out.println(person.getAge());
+            Person person1 = new Person("Test1", 30);
+            Person person2 = new Person("Test2", 35);
+            Person person3 = new Person("Test3", 40);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
