b3548b04fa79fe23ed4d86d1ba2844b83f97589c
Zafarjon <idk677799@gmail.com>
2024-07-16 09:13:48 +0300

    9 One-to-One

--- src/main/java/com/zafar/springCourse/App.java
+++ src/main/java/com/zafar/springCourse/App.java
@@ -1,32 +1,22 @@
 package com.zafar.springCourse;
 
-import com.zafar.springCourse.model.Director;
-import com.zafar.springCourse.model.Movie;
+import com.zafar.springCourse.model.Passport;
+import com.zafar.springCourse.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Director.class)
-                .addAnnotatedClass(Movie.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Passport.class)
+                .addAnnotatedClass(Person.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
         try {
             session.beginTransaction();
 
-            Director director = new Director("Charlie", 30);
 
-            director.addMovie(new Movie("Yoo", 1980));
-            director.addMovie(new Movie("Yoo 2", 1989));
-            director.addMovie(new Movie("Yoo 3", 1999));
-
-            session.save(director);
 
             session.getTransaction().commit();
         } finally {
---
+++ src/main/java/com/zafar/springCourse/model/Person.java
@@ -0,0 +1,63 @@
+package com.zafar.springCourse.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Cascade;
+
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @OneToOne(mappedBy = "person")
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    public Passport passport;
+
+    public Person() {}
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
